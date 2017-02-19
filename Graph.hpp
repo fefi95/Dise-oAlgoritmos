480f7ac22952979d266a0a1713c59d68adbf85b6
Direnzo9330 <direnzosalvatore@gmail.com>
2017-02-18 23:55:20 -0400

    Clase graph ahora clasifica los arcos (requiered y non rquiered)

--- Graph.hpp
+++ Graph.hpp
@@ -11,6 +11,8 @@
 #define _GRAPH_HPP
 
 #include <list>
+#include <iostream>
+using namespace std;
 
 /**************************************************************************//**
  *  GraphInfo. Information of an edge of the graph, it's benefit and cost.
@@ -23,39 +25,108 @@ class GraphInfo {
     public:
         GraphInfo(int, int, int, int);
 
+        //Prints the current Edge Info
+        void Print();
+
+        int get_cost() {return this->ce;}
+
+        int get_benefit() {return this->be;}
+
         // Benefit of crossing an edge once
         int crossEdgeOnce()  { return be - ce; }
 
         // Benefit of crossing an edge twice
         int crossEdgeTwice() { return be - 2 * ce; }
+
 };
 
 // Constructor
-GraphInfo::GraphInfo (int v1, int v2, int be, int ce) {
+GraphInfo::GraphInfo (int v1, int v2, int ce, int be) {
   this->v1 = v1;
   this->v2 = v2;
   this->be = be;
   this->ce = ce;
 }
 
+void GraphInfo::Print(){
+
+	cout << "Start: " << this->v1 << " End: " << this->v2 << " Cost: " <<
+	this->ce << " Benefit: "<< this->be << endl;
+
+}
+
+//Aux function to specify the order to sort a list
+//Does not form part of the namesapce GraphInfo
+bool comp (GraphInfo &a, GraphInfo &b) {
+
+	return ((a.get_benefit() - a.get_cost()) > (b.get_benefit() - b.get_cost()));
+
+}
+
+
+
 /**************************************************************************//**
  *  Graph. List of the edges and it's benefits and costs
  *****************************************************************************/
 class Graph {
     private:
-        std::list<GraphInfo> edges;
+		int nodes;  //Number of nodes of the Graph
+		int r_size; //Number of required edges of the Graph
+		int n_size; //Number of non required edges of the Graph
+        std::list<GraphInfo> r_edges; //List of required edges
+        std::list<GraphInfo> n_edges; //List of non required edges
     public:
-        Graph(std::list<GraphInfo> & edges);
+        Graph(int nodes, int r_size, int n_size, std::list<GraphInfo> & r_edges, std::list<GraphInfo> & n_edges);
         //FUNCIONES QUE PODRIA TENER, AUN NO ESTOY SEGURA
-        void MST();
+        void Print();
+        std::list<GraphInfo> MST();
         void isEulerian();
         void isConnected();
         void connectedComponents();
 };
 
 // Constructor
-Graph::Graph (std::list<GraphInfo> & edges) {
-    this->edges = edges;
+Graph::Graph (int nodes, int r_size, int n_size, std::list<GraphInfo> & r_edges, std::list<GraphInfo> & n_edges) {
+
+	this->nodes = nodes;
+	this->r_size = r_size;
+	this->n_size = n_size;
+	this->r_edges = r_edges;
+	this->n_edges = n_edges;
+	//we proceed to sort the lists for easy work later
+	this->r_edges.sort(comp);
+	this->n_edges.sort(comp);
+}
+
+//Prints the current Graph Info
+void Graph::Print(){
+
+	cout << "Number of Nodes: " << this->nodes << endl;
+	cout << "Required edges: " << this->r_size << endl;
+	for(std::list<GraphInfo>::iterator it= this->r_edges.begin(); it != this->r_edges.end();++it){
+
+		it->Print();
+
+	}
+	cout << "Non Required edges: " << this->n_size << endl;
+	for(std::list<GraphInfo>::iterator it= this->n_edges.begin(); it != this->n_edges.end();++it){
+
+		it->Print();
+
+	}
+}
+
+//Creates the Maximun Spanning Tree (Variation Of Kruskall)
+std::list<GraphInfo> Graph::MST(){
+
+		std::list<GraphInfo> result; //Variable for the returning
+		int visited_nodes[this->nodes] = {0}; //Array for the control of the visited nodes
+
+		//YA LAS LISTAS ESTAN ORDENADAS DONDE POR BENEFICIO MAYOR A COSTO
+		//NO SE SI DEJARLO ASI U ORDENARLOS POR BENEFICIO MAYOR A 2 VECES COSTO
+
+
+
 }
 
 #endif // _GRAPH_HPP
